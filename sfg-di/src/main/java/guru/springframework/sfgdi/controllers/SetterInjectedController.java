6878c2a406c9f6ed861ab916ddf4ad29d824ecf6
Ashkan Ansarifard <ansarifardashkan@gmail.com>
2023-05-29 11:33:04 +0200

    Adds @Qualifiers for multiple beans

--- sfg-di/src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
+++ sfg-di/src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -2,6 +2,7 @@ package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -13,7 +14,7 @@ public class SetterInjectedController {
         return greetingService.sayGreeting();
     }
     @Autowired
-    public void setGreetingService(GreetingService greetingService) {
+    public void setGreetingService(@Qualifier("setterInjectedGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 }
