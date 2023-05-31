75ee892dae8e9865fe715456b644a65432120448
Ashkan Ansarifard <ansarifardashkan@gmail.com>
2023-05-31 09:46:47 +0200

    Adds pet clinic assignment

---
+++ sfg-di-assignment-petclinic/src/test/java/guru/spring5framework/sfgdiassignmentpetclinic/SfgDiAssignmentPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.spring5framework.sfgdiassignmentpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiAssignmentPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
