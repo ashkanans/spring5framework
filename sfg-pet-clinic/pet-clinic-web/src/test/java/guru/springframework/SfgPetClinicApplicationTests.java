3f556d5528dde63ee835b8844d3ce48cc86020d2
Ashkan Ansarifard <ansarifardashkan@gmail.com>
2023-06-08 09:12:56 +0200

    Resolves issues in pet-clinic project

--- sfg-pet-clinic/pet-clinic-web/src/test/java/guru/springframework/SfgPetClinicApplicationTests.java
+++ sfg-pet-clinic/pet-clinic-web/src/test/java/guru/springframework/SfgPetClinicApplicationTests.java
@@ -1,5 +1,7 @@
 package guru.springframework;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
